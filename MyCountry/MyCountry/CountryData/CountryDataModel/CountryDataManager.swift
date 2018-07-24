c0a1b41e4f2a139d4b49e624a3e5fe8c2ee681de
Dimil T Mohan <dimilmhn@gmail.com>
2018-07-24 20:52:42 +0530

    Created the base folder structure and classe

---
+++ MyCountry/MyCountry/CountryData/CountryDataModel/CountryDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  CountryDataManager.swift
+//  MyCountry
+//
+//  Created by Dimil T Mohan on 7/24/18.
+//  Copyright © 2018 Dimil T Mohan. All rights reserved.
+//
+
+import Foundation
---
+++ MyCountry/MyCountry/CountryData/CountryDataPresenter/CountryDataPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  CountryDataPresenter.swift
+//  MyCountry
+//
+//  Created by Dimil T Mohan on 7/24/18.
+//  Copyright © 2018 Dimil T Mohan. All rights reserved.
+//
+
+import Foundation
