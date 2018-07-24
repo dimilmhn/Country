72c1818329200c3214e594de157f8c0675f63bd2
Dimil T Mohan <dimilmhn@gmail.com>
2018-07-24 21:05:30 +0530

    Integrated Cocoapods and Created the Utility and Core Structure

---
+++ MyCountry/MyCountry/Utility/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ItsMyContry
+//
+//  Created by Dimil T Mohan on 7/24/18.
+//  Copyright © 2018 Dimil T Mohan. All rights reserved.
+//
+
+import Foundation
+struct APP_URLS {
+    static let Country_Data_URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+}
